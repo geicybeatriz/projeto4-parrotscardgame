d396fa24e748cac0b893213e7c1a191c333f0673
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-02-01 22:51:48 -0300

    header layout

---
+++ script.js
@@ -0,0 +1,10 @@
+iniciarTela();
+
+function iniciarTela(){
+    const tela = document.querySelector('body');
+    tela.innerHTML = `
+        <header>
+            <h1>PARROT CARD GAME</h1>
+        </header>
+    `;
+}
